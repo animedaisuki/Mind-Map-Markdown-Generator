3d610021c53ec038e4f156235c2cdd4d9c4421b5
wendy <s4594789@student.uq.edu.au>
2024-08-21 00:38:41 +1000

    add selection in command line

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ load_dotenv()
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
 
-def format_description(description):
+def format_description(description, formatter):
     llm = ChatOpenAI(model_name="gpt-4o", temperature=0)
-    prompt = load_prompt("criteria_formater")
+    prompt = load_prompt(formatter)
     system_message = SystemMessage(content=prompt)
     human_message = HumanMessage(description)
 
@@ -43,9 +43,24 @@ def generate_markdown(formatted_description):
 
 
 def main():
-    description = input("请输入需要生成Markdown的内容： ")
+    print("请选择使用的formatter（输入1/2/3）\n1.generative formater\n2.criteria formatter (针对评分标准)\n3.跳过")
+    formatter = input(">")
+    if int(formatter) == 1:
+        formatter = 'generative_formater'
+    elif int(formatter) == 2:
+        formatter = 'criteria_formater'
+    else:
+        formatter = None
 
-    formatted_description = format_description(description)
+    print("请输入需要生成Markdown的内容")
+    description = input(">")
+
+    if not formatter:
+        markdown = generate_markdown(description)
+        U.dump_text(markdown, "markdown/mind_map.md")
+        return
+
+    formatted_description = format_description(description, formatter)
     markdown = generate_markdown(formatted_description)
 
     U.dump_text(markdown, "markdown/mind_map.md")
